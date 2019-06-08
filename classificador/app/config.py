04536fda949a89f19495aa1b3540ded975409253
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2019-06-08 17:09:40 -0300

    inicio firebase

--- classificador/app/config.py
+++ classificador/app/config.py
@@ -3,3 +3,4 @@ import os
 
 LOCAL_DATASET = os.environ['LOCAL_DATASET']
 DATABASE = os.environ['DATABASE']
+API_KEY = os.environ['API_KEY']
---
+++ classificador/app/servico/firebase.py
@@ -0,0 +1,14 @@
+import pyrebase
+
+from app.config import API_KEY
+
+config = {
+  "apiKey": API_KEY,
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
+
+
+if __name__ == '__main__':
+    pass
