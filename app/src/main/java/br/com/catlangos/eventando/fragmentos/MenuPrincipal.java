afc50243cdeeb8f97b35e67f75579bc78f40c1ac
Douglas Ammirante <douglas_ammirante@hotmail.com.br>
2019-06-10 22:44:34 -0300

    Adicionado acesso rapido ao cadastrar evento a partir da tela principal

--- app/src/main/java/br/com/catlangos/eventando/fragmentos/MenuPrincipal.java
+++ app/src/main/java/br/com/catlangos/eventando/fragmentos/MenuPrincipal.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import br.com.catlangos.eventando.R;
+import br.com.catlangos.eventando.evento.CriarEventoFragment;
 import br.com.catlangos.eventando.evento.Evento;
 import br.com.catlangos.eventando.evento.VisualizarEventoActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.*;
 
 import java.util.ArrayList;
@@ -24,6 +29,7 @@ public class MenuPrincipal extends Fragment implements MeuViewHolder.OnEventoCli
     private FirebaseDatabase database;
     private DatabaseReference reference;
     List<Evento> eventos = new ArrayList<>();
+    private FloatingActionButton btnCadastrarEvento;
 
     @Nullable
     @Override
@@ -39,6 +45,20 @@ public class MenuPrincipal extends Fragment implements MeuViewHolder.OnEventoCli
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         final MeuViewHolder.OnEventoClickListener ctx = this;
+
+        btnCadastrarEvento = view.findViewById(R.id.btnCadastrarEvento);
+
+        btnCadastrarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fr = new CriarEventoFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fr);
+                fragmentTransaction.commit();
+            }
+        });
+
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
